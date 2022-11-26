0e32403404cbc23c696d158dd05784550755bef8
dadash-hamed98 <62334566+dadash-hamed98@users.noreply.github.com>
2022-11-26 20:25:26 +0330

    add feature(env,database,migration)
    add databasa name in database.php and .env file
    
    write and edit users migration
    
    fix bug(add defaultString value to appServiceProvider)

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
+            $table->string('last_name');
+            $table->string('national_code');
+            $table->string('phone');
+            $table->string('birthday');
+            $table->tinyInteger('gender');
+            $table->tinyInteger('bank_number');
+            $table->unsignedInteger('provience_id');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
